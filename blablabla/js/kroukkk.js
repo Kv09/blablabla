44068e8e2e903cb130f192b213d861ca0314a950
Kv09 <mael.chiaverini@gmail.com>
2018-01-19 09:55:39 +0100

    Nouveauté
    Gestion de la taille de l'écran
    Javascrip utilisation des fleches
    CSS de la scroll bar

--- blablabla/js/kroukkk.js
+++ blablabla/js/kroukkk.js
@@ -1,16 +1,38 @@
-﻿var i = 0;
+﻿$(document).ready(function () {
+    if (!$.browser.webkit) {
+        $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
+    }
+});
+
+$(document).keydown(function (e) {
+    e = e || window.event;
+    var key = e.which ? e.which : event.keyCode;
+    if (key == 37) retour();
+    if (key == 39) suivant();
+});
+
+var i = 0;
 var op = 0;
 
 function suivant() {
     if (i % 3 == 0) {
+        $('.un').css({ zIndex: 0 });
+        $('.deux').css({ zIndex: 1 });
+        $('.trois').css({ zIndex: 0 });
         $('.un').fadeTo("slow", 0);
         $('.deux').fadeTo("slow", 1);
     }
     if (i % 3 == 1) {
+        $('.un').css({ zIndex: 0 });
+        $('.deux').css({ zIndex: 0 });
+        $('.trois').css({ zIndex: 1 });
         $('.deux').fadeTo("slow", 0);
         $('.trois').fadeTo("slow", 1);
     }
     if (i % 3 == 2) {
+        $('.un').css({ zIndex: 1 });
+        $('.deux').css({ zIndex: 0 });
+        $('.trois').css({ zIndex: 0 });
         $('.trois').fadeTo("slow", 0);
         $('.un').fadeTo("slow", 1);
     }
@@ -23,14 +45,23 @@ function retour() {
         i = 2;
     }
     if (i % 3 == 0) {
+        $('.un').css({ zIndex: 1 });
+        $('.deux').css({ zIndex: 0 });
+        $('.trois').css({ zIndex: 0 });
         $('.un').fadeTo("slow", 1);
         $('.deux').fadeTo("slow", 0);
     }
     if (i % 3 == 1) {
+        $('.un').css({ zIndex: 0 });
+        $('.deux').css({ zIndex: 1 });
+        $('.trois').css({ zIndex: 0 });
         $('.deux').fadeTo("slow", 1);
         $('.trois').fadeTo("slow", 0);
     }
     if (i % 3 == 2) {
+        $('.un').css({ zIndex: 0 });
+        $('.deux').css({ zIndex: 0 });
+        $('.trois').css({ zIndex: 1 });
         $('.trois').fadeTo("slow", 1);
         $('.un').fadeTo("slow", 0);
     }
